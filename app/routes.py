478bf1b6cd2ea1378c188fc7fdd277c3d62b9015
Tyler OBrien <tylerob65@gmail.com>
2023-04-17 17:55:38 -0400

    Experimenting with battling

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import app
 from app.forms import PokeSearchForm
-from app.models import User, PokeFinder
+from app.models import User, PokeFinder, Pokemon
 from flask import redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from app.thepokedex import Pokedex
 import time
+import random
 
 @app.route('/')
 def homePage():
@@ -63,6 +64,9 @@ def myProfilePage():
 
 @app.route('/runcode',methods=['GET'])
 def runCode():
+    p1 = PokeFinder.find_poke("porygon-z")
+    p2 = PokeFinder.find_poke("arctozolt")
+    a = battle_test(p1,p2)
     # Used to test short bits of code
     
     # Shuffles every user's pokemon
@@ -70,13 +74,19 @@ def runCode():
     #     pokemon_name = Pokedex.nums2names[i]
     #     print(i,pokemon_name)
     #     poke_results = PokeFinder.find_poke(pokemon_name)
+    # poke_list = []
+    # all_pokemon = Pokemon.query.all("id")
+    # print(user1)
+    # print(sys.getsizeof(user1))
 
 
-    all_users = User.query.all()
-    for user in  all_users:
-        random_pokemon = Pokedex.pick_random_pokemon(5)
-        user.setRoster(random_pokemon)
-        user.saveToDB()
+
+
+    # all_users = User.query.all()
+    # for user in  all_users:
+    #     random_pokemon = Pokedex.pick_random_pokemon(5)
+    #     user.setRoster(random_pokemon)
+    #     user.saveToDB()
     return redirect(url_for('homePage'))
     
 
@@ -100,3 +110,50 @@ def showProfile(user_id):
         pokemon_name = Pokedex.nums2names[poke_id]
         poke_results_group.append(PokeFinder.find_poke(pokemon_name))
     return render_template('profile.html',poke_results_group=poke_results_group,username=user_profile.username)
+
+
+def battle_test(pokedict1,pokedict2):
+    def damage_each_turn(attacker,defender):
+        power = 10
+        level = 50
+        section1 = (2*level)/5
+        section2 = attacker['attack']/defender['defense']
+        return int(((section1*power*section2)/50)+2)
+    
+
+    # e = effective
+    
+    pokedict1["effective-attack"] = (pokedict1["attack"]+pokedict1["special-attack"])//2
+    pokedict1["effective-defense"] = (pokedict1["defense"]+pokedict1["special-defense"])//2
+    pokedict2["effective-attack"] = (pokedict2["attack"]+pokedict2["special-attack"])//2
+    pokedict2["effective-defense"] = (pokedict2["defense"]+pokedict2["special-defense"])//2
+    pokedict1["turn-damage"] = damage_each_turn(pokedict1,pokedict2)
+    pokedict2["turn-damage"] = damage_each_turn(pokedict2,pokedict1)
+
+    print(pokedict1['poke_id'],pokedict1['name'],"hp",pokedict1['hp'],"effective-attack",pokedict1['effective-attack'],"effective-defense",pokedict1['effective-defense'],"turn-damage",pokedict1["turn-damage"])
+    print(pokedict2['poke_id'],pokedict2['name'],"hp",pokedict2['hp'],"effective-attack",pokedict2['effective-attack'],"effective-defense",pokedict2['effective-defense'],"turn-damage",pokedict2["turn-damage"])
+    pokedict1["health-left"] = pokedict1['hp']
+    pokedict2["health-left"] = pokedict2['hp']
+    
+    
+    if pokedict1['speed'] > pokedict2['speed']:
+        attacker, defender = pokedict1, pokedict2
+    elif pokedict1['speed'] < pokedict2['speed']:
+        attacker, defender = pokedict2, pokedict1
+    else:
+        shuffler = [pokedict1,pokedict2]
+        random.shuffle(shuffler)
+        attacker = shuffler[0]
+        defender = shuffler[1]
+    
+    round_num = 0
+    while round_num < 200:
+        defender['health-left'] -= attacker['turn-damage']
+        if defender['health-left'] <= 0:
+            print("winner",attacker["name"],"rounds",round_num)
+            return attacker
+        
+        attacker, defender = defender, attacker
+        round_num += 1
+    return "Tie"
+
